7e28baf28310fbe50254f2cdcdd3aa5c863bb15f
perryking <perry.king@genesis.global>
2024-10-01 17:26:17 +0100

    Sync to genesiscommunity

---
+++ server/kafka-pipelines-app/src/test/java/global/genesis/Authentication.java
@@ -0,0 +1,7 @@
+package global.genesis;
+
+public class Authentication {
+    public static String SESSION_AUTH_TOKEN = "";
+    public final static String LOGIN_SCHEMA_PATH = "schemas/LoginSchema.json";
+    public static String USERNAME = "";
+}
